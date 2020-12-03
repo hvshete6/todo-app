2ed8afb1257cf42f1bd6f9dc3ea4e1a63e441925
Harshvardhan Shete <hvshete6@gmail.com>
2020-12-04 00:25:45 +0530

    first commit

---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+
+import {Todo} from "../../model/Todo";
+
+import {u4 as uuidv4} from "uuid";
+import { TodoService } from 'src/app/service/todo.service';
+
+
+@Component({
+  selector: "app-todo-form",
+  templateUrl: "./todo-form.component.html",
+  styleUrls: ["./todo-form.component.css"],
+})
+export class TodoFormComponent implements OnInit {
+  todoTitle: string;
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+  }
+
+  handleAdd(){
+    const newTodo: Todo = {
+      id: uuidv4,
+      title: this.todoTitle,
+      isCompleted: false,
+      date: new Date()
+
+    }
+    this.todoService.addTodo(newTodo);
+    this.todoTitle = ""; //so that the valur that we are setting up in the form is empty
+  }
+
+}
